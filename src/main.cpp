15f9a47804eba4ea8f70f6853342c02e6a998cac
efealgan <er.efesal@gmail.com>
2026-04-23 22:22:39 +0300

    Fixed include statements to prioritize submodule includes, made modifications to CMakeLists.txt files so CMake finds and links against SDL submodules, not SOs in /usr/lib.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
-#include "SDL3/SDL_pixels.h"
-#include "SDL3/SDL_render.h"
+#include <SDL3/SDL_pixels.h>
+#include <SDL3/SDL_render.h>
 #include <SDL3/SDL_init.h>
 #include <SDL3/SDL_scancode.h>
 #define SDL_MAIN_USE_CALLBACKS 1  /* use the callbacks instead of main() */
